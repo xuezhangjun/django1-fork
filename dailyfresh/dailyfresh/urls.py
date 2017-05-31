bc025443fddbff813de36bf0e4c71460d70fd628
itcast <xuezhangjun0121@sina.com>
2017-05-31 10:52:19 +0800

    配置用户模块url-解决冲突后

--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^',include('df_goods.urls')),
+
     url(r'^user/',include('df_user.urls')),
+
+    url(r'^',include('df_goods.urls')),
 ]
